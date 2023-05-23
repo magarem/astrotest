a17fddc68407901ccde50401d79a3669e57ab54b
marcelo magalhaes <magawebtec@gmail.com>
2023-05-23 19:31:30 -0300

    ssr with adpter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+  integrations: [solidJs()]
+});
